e5d6f053ce8d8f0d9ec0ef3076b9e37442428db7
Weshley-FM <1juststupid.acc@gmail.com>
2025-03-13 10:52:48 +0700

    update file

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ShopController;
 Route::get('/', function () {
     return redirect()->route('products.index');
 });
 
 Route::resource('products', ProductController::class);
+//Marketplace
+Route::get('/shop', [ShopController::class, 'index'])->name('shop');
+Route::post('/cart/add/{id}', [ShopController::class, 'addToCart'])->name('cart.add');
+
+//Dashboard user
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
